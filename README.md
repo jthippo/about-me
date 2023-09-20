27391f632dbfe154d8564cf4594eb08334a461f1
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-09-20 16:00:16 +0100

    Add top 10 list and Q6

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # about-me
 
+#### LAB 02
+
 [Lighthouse accessibility result: 90%](./lighthouse-mike-foster.png)
+
+#### LAB 03
+
+- Added Top 10 list
+- Added question 6
--- app.js
+++ app.js
@@ -4,6 +4,12 @@ let newUser = prompt(
   "Oh, hello. You've caught me in the middle of coding a webpage. What's your name?"
 );
 
+while (!newUser) {
+  newUser = prompt(
+    "Don't hit cancel. Don't leave the box blank. I'm being welcoming here, so just tell me your name."
+  );
+}
+
 alert(
   "Hello " +
     newUser +
@@ -16,18 +22,16 @@ function doQuiz() {
       newUser +
       "! Put out that cigarette and prepare to answer 5 true or false questions on the topic of Mike Foster!"
   );
-
   let question1 = prompt(
     "Mike once walked into a blind person because he didn't see them. Is this true, YES or NO?"
   );
   console.log(question1);
   console.log(question1.toLowerCase());
-
   switch (question1.toLowerCase()) {
     case "yes":
       // console.log(
       alert(
-        "True! This happened in Clacton in the 1990s, which is amongst the worst time and place Mike can think of."
+        "True! This happened in Clacton in the 1990s, which is perhaps the worst time and place Mike can think of."
       );
       break;
     case "no":
@@ -40,14 +44,11 @@ function doQuiz() {
       // console.log(
       alert("Please work on your spelling. Your parents have been contacted.");
   }
-
   let question2 = prompt(
     "Mike had a YouTube channel where he reviewed cult movies in 10 minutes or less. Is this true, YES or NO?"
   );
-
   console.log(question2);
   console.log(question2.toLowerCase());
-
   switch (question2.toLowerCase()) {
     case "yes":
       // console.log(
@@ -63,14 +64,11 @@ function doQuiz() {
       // console.log(
       alert("Please work on your spelling. Your parents have been contacted.");
   }
-
   let question3 = prompt(
     "Mike once picked up a ball of hair off the kitchen floor and ate it. Is this true, YES or NO?"
   );
-
   console.log(question3);
   console.log(question3.toLowerCase());
-
   switch (question3.toLowerCase()) {
     case "yes":
     case "no":
@@ -83,14 +81,11 @@ function doQuiz() {
       // console.log(
       alert("Please work on your spelling. Your parents have been contacted.");
   }
-
   let question4 = prompt(
     "Mike was once asked what he thought about the movie Pulp Fiction (1994). When he turned to see who was asking, it was Uma Thurman, complete with giant grin on her face. Is this true, YES or NO?"
   );
-
   console.log(question4);
   console.log(question4.toLowerCase());
-
   switch (question4.toLowerCase()) {
     case "yes":
       // console.log(
@@ -108,14 +103,11 @@ function doQuiz() {
       // console.log(
       alert("Please work on your spelling. Your parents have been contacted.");
   }
-
   let question5 = prompt(
     "Mikes primary incentive to learn to drive is that it will allow him to drive across America, trying regional burger variations on the way. Is this true, YES or NO?"
   );
-
   console.log(question5);
   console.log(question5.toLowerCase());
-
   switch (question5.toLowerCase()) {
     case "yes":
       // console.log(
@@ -132,5 +124,31 @@ function doQuiz() {
     default:
       // console.log(
       alert("Please work on your spelling. Your parents have been contacted.");
+  }
+  let guesses = 4;
+  let question6 = prompt(
+    "Mike is thinking of a number between 1 and 13. If you're so smart, tell him what it is. You have " +
+      guesses +
+      " attempts remaining."
+  );
+  while (question6 != 8) {
+    if (question6 < 8) {
+      guesses--;
+      question6 = prompt(
+        "Well that's just too low. You have " + guesses + " attempts remaining."
+      );
+    } else if (question6 > 8) {
+      guesses--;
+      question6 = prompt(
+        "Too high! You have " + guesses + " attempts remaining."
+      );
+    }
+    if (guesses === 1) {
+      alert("You ran out of guesses. It was 8! So obvious, duh.");
+      break;
+    }
+  }
+  if (question6 == 8) {
+    alert("Woah, Psychic " + newUser + " over here! 8 is correct.");
   }
 }
