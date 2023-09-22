5ebc3dc74658b4fdefcabfa1a1927498a5674e15
MIKE FOSTER <HELLOMIKEFOSTER@GMAIL.COM>
2023-09-22 15:45:20 +0100

    Add question 6 and 7 elements

--- README.md
+++ README.md
@@ -13,3 +13,10 @@
 
 - Worked more on question 6 (still doesn't work fully but I understand why, just can't figure out a solution)
 - Added question 7
+
+#### LAB 05b
+
+- Worked more on question 6 (found a workaround with Jez, decided to default back to previous working as it was cleaner if still a tiny bit broken)
+- Worked more on question 7 (lacking all the requested features but the array lookup works
+- Adding point tracking and alert
+  [Lighthouse accessibility result: ](./lighthouse2.png)
--- app.js
+++ app.js
@@ -18,13 +18,11 @@ alert(
 
 function doQuiz() {
   let quizPoints = 0;
-
   alert(
     "Hey, " +
       newUser +
       "! Put out that cigarette and prepare to answer 5 true or false questions on the topic of Mike Foster!"
   );
-
   let question1 = prompt(
     "Mike once walked into a blind person because he didn't see them. Is this true, YES or NO?"
   );
@@ -64,7 +62,6 @@ function doQuiz() {
   let question3 = prompt(
     "Mike once picked up a ball of hair off the kitchen floor and ate it. Is this true, YES or NO?"
   );
-
   switch (question3.toLowerCase()) {
     case "yes":
     case "no":
@@ -80,7 +77,6 @@ function doQuiz() {
   let question4 = prompt(
     "Mike was once asked what he thought about the movie Pulp Fiction (1994). When he turned to see who was asking, it was Uma Thurman, complete with giant grin on her face. Is this true, YES or NO?"
   );
-
   switch (question4.toLowerCase()) {
     case "yes":
       alert(
@@ -100,7 +96,6 @@ function doQuiz() {
   let question5 = prompt(
     "Mikes primary incentive to learn to drive is that it will allow him to drive across America, trying regional burger variations on the way. Is this true, YES or NO?"
   );
-
   switch (question5.toLowerCase()) {
     case "yes":
       alert(
@@ -118,6 +113,10 @@ function doQuiz() {
       alert("Please work on your spelling. Your parents have been contacted.");
   }
 
+  // I can't get question 6 working properly. If you get the right answer on the last attempt, it still says you ran out of guesses. It's because when guesses = 1 and the prompt says "you have 1 more guess" the while loop will stop running, making the final guess pointless. Can't think my way through this one without making the code more complicated and I'm sure there's a more efficient solution.
+
+  //UPDATE: Spoke about this with Jez and we did find a workaround. It was a bit messy though and I think I'd prefer to leave this as it is with it *mostly* working, just that last guess not functional. Next time I try something like this, I'll try a different methodology.
+
   let answer = 8;
   let guesses = 4;
   let question6 = prompt(
@@ -125,44 +124,41 @@ function doQuiz() {
       guesses +
       " attempts remaining."
   );
-
   while (guesses > 1) {
-    guesses--;
     if (question6 == answer) {
-      alert("Woah, Psychic " + newUser + " over here! 8 is correct.");
+      alert("Woah, Psychic" + newUser + " over here! 8 is correct.");
       quizPoints++;
-      console.log(quizPoints);
       break;
     } else if (question6 < answer) {
+      guesses--;
       question6 = prompt(
         "Well that's just too low. You have " + guesses + " attempts remaining."
       );
     } else if (question6 > answer) {
+      guesses--;
       question6 = prompt(
         "Too high! You have " + guesses + " attempts remaining."
       );
     }
   }
-  // I can't get this working properly. If you get the right answer on the last attempt, it still says you ran out of guesses. It's because when guesses = 1 and the prompt says "you have 1 more guess" the while loop will stop running, making the final guess pointless. Can't think my way through this one without making the code more complicated and I'm sure there's a more efficient solution.
 
-  if (guesses == 0) {
+  if (guesses <= 1) {
     alert("You ran out of guesses. It was 8! So obvious, duh.");
   }
 
-  let bhutanFlag = ["yellow", "orange", "white"];
+  // Question 7 is just a bit too much for me to think about thusfar. I've tried adding in code to handle the player entering incorrect entries but it breaks every time so that only "yellow" is an acceptable answer. As above, I'll come back to this sort of thing later I'm sure. I really feel like loops are the sort of things I'll love once I have mastery over them, I love logic and using information in direct ways. It really reminds me of playing guitar, where it starts off impossible and then becomes autopilot with practice.
 
+  let bhutanFlag = ["yellow", "orange", "white"];
   let question7 = prompt(
     "Mike has a fascination with Bhutan. Name one of the three colours in the Bhutanese flag RIGHT NOW."
   ).toLowerCase();
-
-  // I have no idea how to do "else" statements here for when people get the answer wrong, if that's even the solution here!
 
   for (let i = 0; i < bhutanFlag.length; i++) {
     if (question7 == bhutanFlag[i]) {
       alert(
         "Nicely done, " +
           newUser +
-          "! You guessed " +
+          " ! You guessed " +
           question7 +
           " and the Bhutanese flag consists of " +
           bhutanFlag[0] +
@@ -173,8 +169,11 @@ function doQuiz() {
           " dragon in the middle holding some serious jewels."
       );
       quizPoints++;
-      console.log(quizPoints);
     }
   }
-  alert("You got a total of " + quizPoints + " out of 7.");
+  alert(
+    "That's the end of the quiz, newUser. You got a total of " +
+      quizPoints +
+      " questions out of 7 correct. Your certificate is in the mail. Annoyingly, it's been sent to your old address."
+  );
 }
